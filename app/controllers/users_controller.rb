3265f5f4a684700a263e28faecf5583ea89d91a8
Ubuntu <ubuntu@ip-172-31-42-35.ap-northeast-1.compute.internal>
2021-10-12 07:40:32 +0000

    user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,32 @@
 class UsersController < ApplicationController
-
-  def show
+  def show # ユーザの詳細ページのルーティング
+    @user = User.find(params[:id])
   end
 
-  def new
+  def new # ユーザ登録用フォームのルーティング
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = "ユーザーを登録しました"
+      redirect_to @user
+    else
+      flash.now[:danger] = "ユーザーの登録に失敗しました"
+      render :new
+    end
   end
   
   def destroy # ユーザを削除する機能
     find_user_id
     @user.destroy
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password) # name, email, passwordを許可
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: "toppages#index" # トップページのルーティング
   
   get "signup", to: "users#new" # ユーザ登録ページのURLパターンをsignupにする設定
+  
   resources :users, only: [:show, :create]
 end
